d00991aaf34185b1249954d8de59854fe175f278
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-02 12:20:56 -0300

    chore: config setup testing-library and jest

---
+++ src/app.spec.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import '@testing-library/jest-dom'
+
+describe('Jest', () => {
+  it('should work', () => {
+    expect(1).toBe(1)
+  })
+
+  it('work2', () => {
+    expect(1).toBe(1)
+  })
+})
