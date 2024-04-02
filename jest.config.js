c1b75dceb65696c72d68f33945657e0ec5b4fbb0
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-02 16:00:05 -0300

    chore: config babel with ts

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
-/* eslint-disable prettier/prettier */
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.ts'],
-  // moduleNameMapper: {
-  //   '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/.jest/mocks/fileMock.js',
-  //   '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-  // },
 }
