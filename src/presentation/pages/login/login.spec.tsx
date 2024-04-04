f667ec570642d8070bda83b00a10f6bfb05a381c
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-04 19:43:58 -0300

    test: add ensure validade login component

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,21 +1,42 @@
-import { RenderResult, render } from '@testing-library/react'
+import {
+  RenderResult,
+  render,
+  fireEvent,
+  cleanup,
+} from '@testing-library/react'
 import { Login } from './login'
+import { ValidationSpy } from '@/presentation/protocols/validation'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<Login />)
-  return { sut }
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation={validationSpy} />)
+  return { sut, validationSpy }
 }
 
 describe('Login Component', () => {
+  afterEach(cleanup)
   test('Should start initial state', () => {
     const { sut } = makeSut()
     const helperText = sut.queryByTestId('helper-text')
     expect(helperText).toBeNull()
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+  })
+
+  test('Should call validation with correct values', () => {
+    const { sut, validationSpy } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_pass' } })
+    expect(validationSpy.input).toEqual({
+      email: 'any_email',
+      password: 'any_pass',
+    })
   })
 })
