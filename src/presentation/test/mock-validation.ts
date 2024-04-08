dc90134b890aa75bbac28dc3667d9b1e3f39dbdc
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-08 13:38:55 -0300

    test: change Login page tests remove duplication codes

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -1,10 +1,11 @@
+/* eslint-disable unused-imports/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { IValidation } from '@/presentation/protocols/validation'
 
 export class ValidationStub implements IValidation {
   errorMessage: string
 
-  validade(fieldName: string, fieldValue: string): string {
-    console.log(fieldName, fieldValue)
+  validade(_fieldName: string, _fieldValue: string): string {
     return this.errorMessage
   }
 }
