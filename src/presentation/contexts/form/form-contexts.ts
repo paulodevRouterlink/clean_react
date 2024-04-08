4c907bb12ab886839cbb987e74c6bf6afa883975
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-08 17:04:37 -0300

    refactor: change contexts form

--- src/presentation/contexts/form/form-contexts.ts
+++ src/presentation/contexts/form/form-contexts.ts
@@ -1,3 +1,12 @@
 import { createContext } from 'react'
 
-export const FormContext = createContext(null)
+type FormContextProps = {
+  isLoading: boolean
+  email: string
+  password: string
+  emailError: string
+  passwordError: string
+  mainError: string
+}
+
+export const FormContext = createContext({} as FormContextProps | null)
