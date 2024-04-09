6947761cf07b8b526e66bf9197ac66a8626b9919
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-09 15:13:26 -0300

    feat: ensure EmailValidation returns if false is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -1,10 +1,13 @@
 import { IFieldValidation } from '@/validation/protocols'
 import { ValidateError } from '@/validation/errors'
+import { Regex } from '../regex'
 
 export class EmailValidation implements IFieldValidation {
   constructor(readonly field: string) {}
 
   validate(value: string): Error {
-    return new ValidateError.InvalidFieldError()
+    return Regex.validateEmail(value)
+      ? null
+      : new ValidateError.InvalidFieldError()
   }
 }
