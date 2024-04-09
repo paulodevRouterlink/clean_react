eb1b3b01ac54b468c77b6cfdf78297934bddc6a6
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-09 13:05:15 -0300

    test: ensure Required field validation return false

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredError } from '@/validation/errors'
 import { RequiredFieldValidation } from './required-field-validation'
+import { faker } from '@faker-js/faker'
 
 describe('Required Field Validation', () => {
   test('Should return error if field is empty', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredError.FieldError())
+  })
+
+  test('Should return error if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
