3bf43d90dab280d278bb4fd470e7e63462a0efcd
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-09 13:19:18 -0300

    refactor: move SUT creation factory method

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -2,15 +2,16 @@ import { RequiredError } from '@/validation/errors'
 import { RequiredFieldValidation } from './required-field-validation'
 import { faker } from '@faker-js/faker'
 
+const makeSut = () => new RequiredFieldValidation(faker.database.column())
 describe('Required Field Validation', () => {
   test('Should return error if field is empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new RequiredError.FieldError())
   })
 
   test('Should return error if field is not empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = makeSut()
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
