6c3504a570a61307961411019b4867be78be44ea
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-09 15:39:40 -0300

    refactor: move SUT creation factory method

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,15 +1,17 @@
 import { ValidateError } from '@/validation/errors'
 import { MinLengthValidation } from './min-length-validation'
 
+const makeSut = () => new MinLengthValidation('field', 5)
+
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
-    const sut = new MinLengthValidation('field', 5)
+    const sut = makeSut()
     const error = sut.validate('123')
     expect(error).toEqual(new ValidateError.InvalidFieldError())
   })
 
   test('Should return falsy if value is valid', () => {
-    const sut = new MinLengthValidation('field', 5)
+    const sut = makeSut()
     const error = sut.validate('12345')
     expect(error).toBeFalsy()
   })
