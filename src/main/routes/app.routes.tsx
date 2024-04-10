b98f4dbe15696fc266ebaf584c6092e7683bc651
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-10 18:46:28 -0300

    refactor: change file routes

--- src/main/routes/app.routes.tsx
+++ src/main/routes/app.routes.tsx
@@ -5,14 +5,9 @@ type Props = {
   makeLogin: FC
 }
 
-const appRoutes = ({ makeLogin: Login }: Props): RouteObject => ({
-  path: '/login',
-  element: <Login />,
-})
+const appRoutes = ({ makeLogin: Login }: Props): RouteObject[] => [
+  { path: '/login', element: <Login /> },
+  { path: '/sign-up', element: <h1>sign-up</h1> },
+]
 
-const signUp = (): RouteObject => ({
-  path: '/sign-up',
-  element: <h1>sign-up</h1>,
-})
-
-export { appRoutes, signUp }
+export { appRoutes }
--- src/main/routes/routes.tsx
+++ src/main/routes/routes.tsx
@@ -1,12 +1,11 @@
 import { useRoutes } from 'react-router-dom'
-import { appRoutes, signUp } from './app.routes'
+import { appRoutes } from './app.routes'
 import { makeLogin } from '@/main/factory/pages'
 
 const Routes = () => {
   const mainRoutes = appRoutes({ makeLogin })
-  const authSignUp = signUp()
 
-  return useRoutes([mainRoutes, authSignUp])
+  return useRoutes(mainRoutes)
 }
 
 export { Routes }
