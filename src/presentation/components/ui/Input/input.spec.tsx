08130d4790818c4c90cdb7acda7374771d5be66f
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-10 15:04:01 -0300

    test: ensure Input component

--- src/presentation/components/ui/Input/input.spec.tsx
+++ src/presentation/components/ui/Input/input.spec.tsx
@@ -1,10 +1,22 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { Input } from './input'
+import { faker } from '@faker-js/faker'
+
+const makeSut = (fieldName: string) => render(<Input name={fieldName} />)
 
 describe('InputComponent', () => {
   test('Should begin with readOnly', () => {
-    render(<Input name="field" />)
-    const input = screen.getByTestId('field') as HTMLInputElement
+    const field = faker.database.column()
+    makeSut(field)
+    const input = screen.getByTestId(field) as HTMLInputElement
     expect(input.readOnly).toBe(true)
+  })
+
+  test('Should remove readOnly on focus', () => {
+    const field = faker.database.column()
+    makeSut(field)
+    const input = screen.getByTestId(field) as HTMLInputElement
+    fireEvent.focus(input)
+    expect(input.readOnly).toBe(false)
   })
 })
