6b2044c28388e20bd4bbbab31258d6563a3e26ec
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-10 17:20:00 -0300

    refactor: move SUT creation files

--- src/data/usecases/save-acess-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-acess-token/local-save-access-token.spec.ts
@@ -3,10 +3,24 @@ import { faker } from '@faker-js/faker'
 import { LocalSaveAccessToken } from './local-save-access-token'
 import { SetStorageSpy } from '@/data/test/mock-set-storage'
 
+type SutTypes = {
+  sut: LocalSaveAccessToken
+  setStorageSpy: SetStorageSpy
+}
+
+const makeSut = (): SutTypes => {
+  const setStorageSpy = new SetStorageSpy()
+  const sut = new LocalSaveAccessToken(setStorageSpy)
+
+  return {
+    sut,
+    setStorageSpy,
+  }
+}
+
 describe('LocalSaveAccessToken', () => {
   test('Should call SetStorage with correct with values', async () => {
-    const setStorageSpy = new SetStorageSpy()
-    const sut = new LocalSaveAccessToken(setStorageSpy)
+    const { sut, setStorageSpy } = makeSut()
     const accessToken = faker.string.uuid()
     await sut.save(accessToken)
     expect(setStorageSpy.key).toBe('accessToken')
