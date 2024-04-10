d3f6d40950f26d58e1e127408b7f4a0ddea4c732
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-10 17:49:39 -0300

    refactor: move SUT creation files

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -2,12 +2,22 @@ import 'jest-localstorage-mock'
 import { faker } from '@faker-js/faker'
 import { LocalStorageAdapter } from './local-storage-adapter'
 
+const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter()
+
 describe('LocalStorageAdapter', () => {
   beforeEach(() => {
     localStorage.clear()
   })
   test('Should call localStorage with correct values', async () => {
-    const sut = new LocalStorageAdapter()
+    const sut = makeSut()
+    const key = faker.database.column()
+    const value = faker.word.words()
+    await sut.set(key, value)
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
+  })
+
+  test('Should call localStorage with failure', async () => {
+    const sut = makeSut()
     const key = faker.database.column()
     const value = faker.word.words()
     await sut.set(key, value)
