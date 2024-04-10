fcb2b0ac152089b93801a6a992b1d4102048e8e9
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-10 17:48:02 -0300

    test: ensure LocalStorageAdapter with correct values

---
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -0,0 +1,16 @@
+import 'jest-localstorage-mock'
+import { faker } from '@faker-js/faker'
+import { LocalStorageAdapter } from './local-storage-adapter'
+
+describe('LocalStorageAdapter', () => {
+  beforeEach(() => {
+    localStorage.clear()
+  })
+  test('Should call localStorage with correct values', async () => {
+    const sut = new LocalStorageAdapter()
+    const key = faker.database.column()
+    const value = faker.word.words()
+    await sut.set(key, value)
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
+  })
+})
