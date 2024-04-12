df1420da3b39da016bc5136ae39d9cceaa107dde
pauloWeslleY <weslleyspfc@13outlook.com>
2024-04-12 00:50:42 -0300

    feat: add SignUp validation factory

---
+++ src/main/factory/pages/sign-up/sign-up-validation.spec.ts
@@ -0,0 +1,19 @@
+import {
+  ValidationBuilder as Builder,
+  ValidationComposite,
+} from '@/validation/validators'
+import { makeSignUpValidation } from './sign-up-validation'
+
+describe('SignUpValidateFactory', () => {
+  test('should composite ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation()
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...Builder.field('name').required().min(5).build(),
+        ...Builder.field('email').required().email().build(),
+        ...Builder.field('password').required().min(5).build(),
+        ...Builder.field('passwordConfirmation').required().min(5).build(),
+      ]),
+    )
+  })
+})
