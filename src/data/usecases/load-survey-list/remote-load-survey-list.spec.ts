39229b3c5d97e7ba6bfab00aa56d5a3539d24205
pauloWeslleY <weslleyspfc@13outlook.com>
2024-04-13 07:15:52 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 500

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -41,4 +41,13 @@ describe('RemoteLoadSurveyList', () => {
     const promise = sut.loadAll()
     await expect(promise).rejects.toThrow(new Errors.UnexpectedError())
   })
+
+  test('Should throw UnexpectedError if HttpGetClient return 500', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    }
+    const promise = sut.loadAll()
+    await expect(promise).rejects.toThrow(new Errors.UnexpectedError())
+  })
 })
