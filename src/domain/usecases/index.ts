17777db069bbfa82a1b9af85fb9478c23ffea304
pauloWeslleY <weslleyspfc@13outlook.com>
2024-04-13 05:52:23 -0300

    feat: add LoadSurveyList usecase

--- src/domain/usecases/index.ts
+++ src/domain/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from './auth'
 export * from './add-account'
 export * from './save-access-token'
+export * from './load-survey-list'
---
+++ src/domain/usecases/load-survey-list/load-survey-list.ts
@@ -0,0 +1,5 @@
+import { SurveyModel } from '@/domain/models'
+
+export interface ILoadSurveyList {
+  loadAll(): Promise<SurveyModel>
+}
