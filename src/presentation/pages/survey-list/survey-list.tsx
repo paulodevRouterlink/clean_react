657fdd605fb644207b74afe13e229206102010d5
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-15 16:08:54 -0300

    feat: move header its own file

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,23 +1,16 @@
 import { FC } from 'react'
-import { Footer } from '@/presentation/components/layout'
-import { CardSurvey, HeaderSurvey } from './components'
+import { CardSurvey } from './components'
 import Styled from './survey-list.module.scss'
 
 const SurveyList: FC = () => {
   return (
-    <div className={Styled.survey_list}>
-      <HeaderSurvey />
-
-      <div className={Styled.survey_list__content}>
-        <h2>Enquete</h2>
-        <ul>
-          {Array.from({ length: 3 }).map((_, index) => (
-            <CardSurvey key={index} />
-          ))}
-        </ul>
-      </div>
-
-      <Footer />
+    <div className={Styled.survey_list__content}>
+      <h2>Enquete</h2>
+      <ul>
+        {Array.from({ length: 3 }).map((_, index) => (
+          <CardSurvey key={index} />
+        ))}
+      </ul>
     </div>
   )
 }
