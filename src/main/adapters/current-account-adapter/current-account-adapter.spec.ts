ed1fc9914c092b3d18feeef42c4d9312c18aac31
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-15 19:52:40 -0300

    feat: ensure CurrentAccountAdapter calls localStorageAdapter with correct with values

---
+++ src/main/adapters/current-account-adapter/current-account-adapter.spec.ts
@@ -0,0 +1,14 @@
+import { mockAccountModel } from '@/domain/test'
+import { setCurrentAccountAdapter } from './current-account-adapter'
+import { LocalStorageAdapter } from '@/infra/cache'
+
+jest.mock('@/infra/cache/local-storage-adapter')
+
+describe('CurrentAccountAdapter', () => {
+  test('Should call LocalStorageAdapter with correct values', () => {
+    const account = mockAccountModel()
+    const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
+    setCurrentAccountAdapter(account)
+    expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+})
