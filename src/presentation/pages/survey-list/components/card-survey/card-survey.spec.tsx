23a2e08043d55344622c7c2e498790b8e7e3fa68
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-16 19:17:02 -0300

    test: ensure CardSurvey load icon and day correctly

--- src/presentation/pages/survey-list/components/card-survey/card-survey.spec.tsx
+++ src/presentation/pages/survey-list/components/card-survey/card-survey.spec.tsx
@@ -17,4 +17,15 @@ describe('CardSurvey Component', () => {
     expect(screen.getByTestId('month')).toHaveTextContent('abr')
     expect(screen.getByTestId('year')).toHaveTextContent('2024')
   })
+
+  test('Should render with correct values', () => {
+    const survey = Object.assign(mockSurveyModel(), {
+      date: new Date('2019-03-03T00:00:00'),
+    })
+    makeSut(survey)
+    expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('03')
+    expect(screen.getByTestId('month')).toHaveTextContent('mar')
+    expect(screen.getByTestId('year')).toHaveTextContent('2019')
+  })
 })
