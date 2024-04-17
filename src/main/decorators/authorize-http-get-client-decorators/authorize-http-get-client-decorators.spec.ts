1a9b40aa126d9cebea50596dc425af40d2305043
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2024-04-17 17:26:28 -0300

    refactor: move SUT creation to a factory method

--- src/main/decorators/authorize-http-get-client-decorators/authorize-http-get-client-decorators.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorators/authorize-http-get-client-decorators.spec.ts
@@ -1,10 +1,30 @@
 import { GetStorageSpy, mockGetRequest } from '@/data/test'
 import { AuthorizeHttpGetClientDecorator } from '@/main/decorators'
 
+type SutTypes = {
+  getStorageSpy: GetStorageSpy
+  sut: AuthorizeHttpGetClientDecorator
+}
+
+const makeSut = (): SutTypes => {
+  const getStorageSpy = new GetStorageSpy()
+  const sut = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+
+  return {
+    sut,
+    getStorageSpy,
+  }
+}
+
 describe('AuthorizeHttpGetClientDecorator', () => {
   test('Should call GetStorage with correct value', () => {
-    const getStorageSpy = new GetStorageSpy()
-    const sut = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+    const { getStorageSpy, sut } = makeSut()
+    sut.get(mockGetRequest())
+    expect(getStorageSpy.key).toBe('account')
+  })
+
+  test('Should not add headers with getStorage is invalid', () => {
+    const { getStorageSpy, sut } = makeSut()
     sut.get(mockGetRequest())
     expect(getStorageSpy.key).toBe('account')
   })
